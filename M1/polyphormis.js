951f9bb46d33906773fafbd662db1c22fe21a439
jakub41 <lemiszewski@gmx.com>
2019-10-08 17:09:46 +0200

    Added .editorconfig to make better the code style

--- M1/polyphormis.js
+++ M1/polyphormis.js
@@ -1,22 +1,21 @@
 /**
- * Generic class Animal 
+ * Generic class Animal
  * **/
 class Animal {
-    // Constructor method Animal Name
-    constructor (name) {
-        this.name = name;
-    }
+  // Constructor method Animal Name
+  constructor(name) {
+    this.name = name;
+  }
 
-    // Method we will override for Animal Sound
-    makeSound() {
-        console.log("Sound of an animal");
-    }
+  // Method we will override for Animal Sound
+  makeSound() {
+    console.log("Sound of an animal");
+  }
 }
 
 // New Instance of the Animal Class
 const a1 = new Animal("Fuffy");
 console.log(a1);
-
 
 // Let the animal make a sound
 a1.makeSound();
@@ -25,15 +24,15 @@ a1.makeSound();
  * SubClass Dog child of Animal Class
  * **/
 class Dog extends Animal {
-    constructor(name) {
-        // Call this constructor from the parent Animal -> Inheritance
-        super(name);
-    }
+  constructor(name) {
+    // Call this constructor from the parent Animal -> Inheritance
+    super(name);
+  }
 
-    // Polymorphism we override the method of the parent makeSound()
-    makeSound() {
-        console.log("Woof! Woof!");
-    }
+  // Polymorphism we override the method of the parent makeSound()
+  makeSound() {
+    console.log("Woof! Woof!");
+  }
 }
 
 // New Dog instance
@@ -43,19 +42,19 @@ console.log(a2);
 // Dog make a sound
 a2.makeSound();
 
- /**
+/**
  * SubClass Cat child of Animal Class
  * **/
 class Cat extends Animal {
-    constructor(name) {
-        // Call this constructor from the parent Animal -> Inheritance
-        super(name);
-    }
+  constructor(name) {
+    // Call this constructor from the parent Animal -> Inheritance
+    super(name);
+  }
 
-    // Polymorphism we override the method of the parent makeSound()
-    makeSound() {
-        console.log("Miaou! Miaou!");
-    }
+  // Polymorphism we override the method of the parent makeSound()
+  makeSound() {
+    console.log("Miaou! Miaou!");
+  }
 }
 
 // Cat instance
