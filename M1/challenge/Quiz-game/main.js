ab01464e844383abb95261f09862a6793674860d
jakub41 <lemiszewski@gmx.com>
2019-10-18 12:04:50 +0200

    [WIP] Defining the DOM elements & accesibiliyty & data from API

--- M1/challenge/Quiz-game/main.js
+++ M1/challenge/Quiz-game/main.js
@@ -6,7 +6,7 @@
 // Creating a request variable with the OBJ inside
 var request = new XMLHttpRequest();
 // Opening a GET request to the API
-request.open("GET", "https://opentdb.com/api.php?amount=10", true);
+request.open("GET", "https://opentdb.com/api.php?amount=10&difficulty=hard", true);
 // Load the request
 request.onload = function() {
     // Accessing the JSON data
@@ -17,17 +17,27 @@ request.onload = function() {
         // Looping the data
         data.forEach(results => {
             console.log(results.question);
-
+            // Creating the question card
             var card = document.createElement("div");
             card.setAttribute("class", "card");
-
-            var h1 = document.createElement("h1");
+            // Creating the heading using the question
+            var h1 = document.createElement("h4");
             h1.textContent = results.question;
 
-            var p = document.createElement("p");
-            results.correct_answer = results.correct_answer;
-            p.textContent = `${results.correct_answer}`;
+            var incorrect = document.createElement("p").setAttribute("class", "incorrect-answer");
+            data.results.incorrect_answers.foreach( results =>
+                {
+                    var incorrectList = document.createElement(li);
+                    results.incorrect_answers = results.incorrect_answers;
+                    incorrectList.textContent = results.incorrect_answers;
+                });
 
+                incorrect.appendChild(incorrectList);
+            // Creating the correct answer element
+            var correct = document.createElement("p").etAttribute("class", "correct-answer");
+            results.correct_answer = results.correct_answer;
+            correct.textContent = `${results.correct_answer}`;
+            // Append to DOM
             container.appendChild(card);
             card.appendChild(h1);
             card.appendChild(p);
@@ -35,6 +45,9 @@ request.onload = function() {
     } else {
         // Showing error for the status
         console.log("error");
+        var errorMessage = document.createElement("status-error-message");
+        errorMessage.textContent = 'Not working!';
+        quiz.appendChild(errorMessage);
     }
 };
 // Request send
