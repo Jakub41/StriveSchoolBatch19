1f37682aff9be53c283d14d460e3280750c6179d
jakub41 <lemiszewski@gmx.com>
2019-10-30 16:52:15 +0100

    Added tooltips & style

--- M2/exercises/dya6/main.js
+++ M2/exercises/dya6/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+    // Tooltip
+    $("body").tooltip({
+        selector: "[data-toggle='tooltip']",
+        container: "body"
+      });
+
     //  Remove selected table rows
     $(".delete-row").click(function() {
         $(this).closest('tr').remove();
