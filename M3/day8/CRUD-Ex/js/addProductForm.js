7242a027ec7551c21feab3f1b79b33052c2efeec
jakub41 <lemiszewski@gmx.com>
2019-11-14 22:32:16 +0100

    Added style & added comments and fixed add products

--- M3/day8/CRUD-Ex/js/addProductForm.js
+++ M3/day8/CRUD-Ex/js/addProductForm.js
@@ -1,17 +1,34 @@
 console.log("AddProductForm...");
+
+/**
+ *
+ * ! Add Product
+ * * Method to add a product from a form
+ * * and POST it to the server API
+ *
+ * @function addProductsForm
+ * @async
+ * @param { Strings } name, brand, description, image
+ * @param { Number } price
+ *
+ */
+// Button click on the form
 H.$1("#addProductForm .btn").addEventListener("click", e => {
     e.preventDefault();
+    // Values from the form
     const name = H.$1("#name").value;
     const brand = H.$1("#brand").value;
     const description = H.$1("#description").value;
     const price = H.$1("#price").value;
     const image = H.$1("#imageUrl").value;
+    // We call the function to POST the data
     addProductsForm(name, brand, description, price, image);
 });
 
+// POST Function
 addProductsForm = async (name, brand, description, price, image) => {
     console.log("Adding product");
-
+    // We create the product to API
     const response = await Fetch.create("/product", {
         name: name,
         brand: brand,
@@ -19,8 +36,6 @@ addProductsForm = async (name, brand, description, price, image) => {
         price: price,
         imageUrl: image
     });
-
+    // Reset the form on complete
     H.$1("#addProductForm").reset();
-
-    console.log(response);
 };
--- M3/day8/CRUD-Ex/js/getProducts.js
+++ M3/day8/CRUD-Ex/js/getProducts.js
@@ -1,8 +1,20 @@
 console.log("getProducts...");
+/**
+ * ! Get Products
+ * * Method to GET the data from API
+ * * we use the GET exposed from fetchData
+ *
+ * @function getProducts
+ * @async
+ * @param { String } URL
+ *
+*/
 const getProducts = async () => {
+    // We fetch all products GET method
     const products = await Fetch.get("/product");
     console.log("products...", products);
+    // Return the data
     return products;
 };
-
+// Init the function
 getProducts();
