0d4523676a30ae0151188f70ab7b42262bef0d85
jakub41 <lemiszewski@gmx.com>
2019-11-14 14:01:35 +0100

    Added better comments and started the showAll product part

--- M3/day8/CRUD-Ex/js/showProducts.js
+++ M3/day8/CRUD-Ex/js/showProducts.js
@@ -1,20 +1,33 @@
-initAllProducts = async () => {
+initAllProducts = async() => {
     let allProducts = await getProducts();
     console.log("All products...", allProducts);
-
     let productsSection = H.$("#products-section");
-    let productRow = H.createNode("div");
-    productRow.className = "row";
-    let productCard = productsSection.append(productRow);
-    productCard.innerHTML = "hello";
-
+    let productRowDiv = H.createNode("div");
+    productRowDiv.className = "row";
+    let productCard = productsSection.appendChild(productRowDiv);
     showAllProduct(allProducts, productCard);
-};
+    };
 
-showAllProduct = (allProducts, productCard) => {
-    productCard.innerHtml = allProducts.map(
-        product => `<h1>${product.name}</h1>`
-    );
-};
+    showAllProduct = (allProducts, productCard) => {
 
-initAllProducts();
+    productCard.innerHTML= allProducts.map(
+    product => `
+    `
+    ).join('');
+
+    };
+
+    initAllProducts();
+/**
+ <div class="col-md-4">
+        <div class="card mb-4 text-white bg-dark">
+            <img class="card-img-top" src="//placeimg.com/290/180/any" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                    content.</p>
+                <a href="http://www.jquery2dotnet.com/" class="btn btn-outline-light btn-sm">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+ */
