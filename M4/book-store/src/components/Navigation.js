12659787086223c8a22ab28d33397b90c2ff3486
jakub41 <lemiszewski@gmx.com>
2019-11-19 21:41:53 +0100

    Collapse icon fix

--- M4/book-store/src/components/Navigation.js
+++ M4/book-store/src/components/Navigation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { GiBookshelf } from "react-icons/gi";
+import { Navbar, Nav } from "react-bootstrap";
 
 const NavItem = props => {
     const pageURI = window.location.pathname + window.location.search;
@@ -22,33 +23,19 @@ const NavItem = props => {
 class Navigation extends React.Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="/home">
-                <GiBookshelf />
-                </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div
-                    className="collapse navbar-collapse"
-                    id="navbarSupportedContent"
-                >
-                    <ul className="navbar-nav mr-auto">
+            <Navbar bg="dark" expand="lg" variant="dark">
+                <Navbar.Brand href="/home">
+                    <GiBookshelf />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
                         <NavItem path="/home" name="Home" />
                         <NavItem path="/About" name="About" />
                         <NavItem path="/Contact" name="Contact" />
-                    </ul>
-                </div>
-            </nav>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         );
     }
 }
